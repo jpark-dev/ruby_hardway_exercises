c1366f747aa80807637963b07271c6a551c8285c
Junsung Park <zeipar@gmail.com>
2020-04-27 07:21:38 +0000

    What if statements

---
+++ ex29.rb
@@ -0,0 +1,45 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats
+  puts "Too many cats! The world is doomed!"
+end
+
+if people > cats
+  puts "Not many cats! The world is saved!"
+end
+
+if people < dogs
+  puts "The world is drooled on!"
+end
+
+if people > dogs
+  puts "The world is dry!"
+end
+
+dogs += 5
+
+if people >= dogs
+  puts "People are greater than or equal to dogs."
+end
+
+if people <= dogs
+  puts "People are less than or equal to dogs."
+end
+
+if people == dogs
+  puts "People are dogs."
+end
+
+if people == true
+  puts "People are true."
+end
+
+if people != true
+  puts "Not all people are true."
+end
+
+if (people)
+  puts "Really true?"
+end

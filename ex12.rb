a88ff66a820a7c9fd45306d68b5ff9e7d55865b0
Junsung Park <zeipar@gmail.com>
2020-04-27 01:00:34 +0000

    Prompting for numbers

---
+++ ex12.rb
@@ -0,0 +1,18 @@
+print "Give me a number: "
+number = gets.chomp.to_i
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_i
+
+smaller = number / 100
+puts "A smaller number is #{smaller}."
+
+print "Float numbers float: "
+number = gets.chomp.to_f
+
+oneTenth = number / 10
+puts "A float number is #{oneTenth}." 

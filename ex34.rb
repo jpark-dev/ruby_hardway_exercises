5219edb1113975ae8a59d73bec72bccea87f8777
Junsung Park <zeipar@gmail.com>
2020-04-27 07:49:46 +0000

    Accessing Elements of Arrays

---
+++ ex34.rb
@@ -0,0 +1,25 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The animal at 1.
+puts "ruby"
+
+# The third (3rd) animal.
+puts "peacock"
+
+# The first (1st) animal.
+puts "bear"
+
+# The animal at 3.
+puts "kangaroo"
+
+# The fifth (5th) animal.
+puts "whale"
+
+# The animal at 2.
+puts "peacock"
+
+# The sixth (6th) animal.
+puts "platypus"
+
+# The animal at 4.
+puts "whale"
